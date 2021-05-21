f93034f063a6a94d3cc1f40295e22dbbffab5cfa
Siddhartha Singh <masterwww03@gmail.com>
2021-05-21 17:49:06 +0530

    Day 2

---
+++ Day2/Primitive_data_types.py
@@ -0,0 +1,44 @@
+#Data Types
+
+#String
+
+# "Hello"[4]
+# print("123" + "345")
+
+#Integer
+
+# print(123 + 345)
+# 123_456_789
+
+#Float
+
+# 3.14159
+
+#Boolean
+
+# True
+# False
+
+##Type Error, Type Checking and Type Conversion
+
+num_char =  len(input("What is your name? : "))
+
+new_num_char = str(num_char)
+print("Your name contains " + new_num_char + " Chacters")
+
+
+
+# a = float(123)
+# print(type(a))
+
+
+# print(70 + float("100.5"))
+# print(str(70)+str(100))
+
+
+
+
+
+
+
+ 
