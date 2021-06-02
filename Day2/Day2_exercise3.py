323f327d504beace156e831182b8356dd6fcffae
Siddhartha Singh <masterwww03@gmail.com>
2021-06-02 17:07:52 +0530

    Day2 Exercise

---
+++ Day2/Day2_exercise3.py
@@ -0,0 +1,28 @@
+## Your Life in Weeks
+
+# I was reading this article by Tim Urban - Your Life in Weeks and realized.
+#  just how little time we actually have.
+
+# Create a program using maths and f-Strings that tells us how many days,
+#  Weeks, Months we have left if we live until 90 years old.
+
+# It Will take your current age as the input and output a message with our 
+#  time left in this format:
+
+#  => You have x Days, y Weeks, and z months left.
+
+# Where x, y and z are replaced with the actual calculated numbers.
+
+# Warning => Your output should match the example output format exactly,
+#             even the positions of the commas and full stops.
+
+## Example Input
+
+# What is your current age? => 56
+
+# OUTPUT
+
+# You have 12410 days, 1768 weeks, and 408 months left.
+
+
+
