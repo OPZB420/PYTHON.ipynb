b1f0f42d8afbf3d876a24269cb1fae29e3a56a8c
Siddhartha Singh <masterwww03@gmail.com>
2021-06-07 18:53:35 +0530

    Random Module

---
+++ Day4/Random_Module.py
@@ -0,0 +1,14 @@
+
+import random
+import My_module
+
+
+random_integer = random.randint(1,10)
+print(random_integer)
+
+print(My_module.pi)
+
+## random float values 0.000000 to 0.99999999...
+## to increase the value multiply a value(*n)
+random_float = random.random() * 5
+print(random_float)
