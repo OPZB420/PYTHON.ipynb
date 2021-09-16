812f96bc822bbe9a0cecd465d24bcfb5f983f0f6
Siddhartha Singh <masterwww03@gmail.com>
2021-09-16 23:00:14 +0530

    grading program project

--- Day9/Grading_program.py
+++ Day9/Grading_program.py
@@ -1,3 +1,5 @@
+## Grading Program Project
+
 ## Instructions
 
 # You have access to a database of student_scores in the formate of a dictionary.
@@ -15,3 +17,31 @@
     # Score 71 - 80 : Grade = "Accetable"
     # Score 70 or lower : Grade = "Fail"
 
+student_scores = {
+    "Harry": 81,
+    "Ron": 78, 
+    "Hermon": 99,
+    "Draco": 74,
+    "Neville": 62,
+ }
+
+#TODO -1: Create an empty dictionary called student_grades.
+student_grades = {}
+
+
+#TODO -2: Write your code below to add the grades to student_grades
+
+for student in student_scores:
+    score = student_scores[student]
+    if score > 90:
+        student_grades[student] = "Outstanding"
+    elif score > 80:
+        student_grades[student] = "Exceeds Expectation"
+    elif score > 70:
+        student_grades[student] = "Accetable"
+    else:
+        student_grades[student] = "Fail"
+        
+
+#Don't change the code below
+print(student_grades)
