6a784e95a71d98061eeeb79030a19cddcbe3ae5e
Siddhartha Singh <masterwww03@gmail.com>
2021-09-16 16:51:11 +0530

    Day8 Caesar Ciphere code

--- Day7/Hangman_2.py
+++ Day7/Hangman_2.py
@@ -1,4 +1,4 @@
-## Hangman Project part-2
+## Hangman Project part-2 
 import random
 
 world_list = ["aardvark", "baboon", "camel"]
@@ -21,21 +21,30 @@ for _ in range(word_len):
     display += "_"
 print(display)
 
-guess = input("Guess a letter: ").lower()
+#-> End of game using while loop 
+
+end_of_game = False
+
+while not end_of_game:
+    guess = input("Guess a letter: ").lower()
 
 #TODO -2 => Loop through each position in the chosen_word;
 #If the letter at that position matches 'guess' then reveal that letter in the display at that position.
 #e.g. If the user guessed "p" and the chosen word was "Apple", then display should be     
 # ["_","p","p","_","_"]
 
-for position in range(word_len):
-    letter = chosen_word[position]
-    if letter == guess:
-        display[position] = letter
-   
+    for position in range(word_len):
+        letter = chosen_word[position]
+        if letter == guess:
+            display[position] = letter
+    
 
-#TODO -3 => Print 'display' and you should see the guessed letter in the correct position and every other letter replace with "_".
+    #TODO -3 => Print 'display' and you should see the guessed letter in the correct position and every other letter replace with "_".
 
-#Hint - Don't worry about getting the user to guess the next letter. 
-# We'll tackle that in step 3.
-print(display)
+    #Hint - Don't worry about getting the user to guess the next letter. 
+    # We'll tackle that in step 3.
+    print(display)
+    
+    if "_" not in display:
+        end_of_game = True
+        print("You won!")
--- Day8/Caesar_Cipher.py
+++ Day8/Caesar_Cipher.py
@@ -1,20 +1,74 @@
-alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z','a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
-
-direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
-text = input("Type your message:\n").lower()
-shift = int(input("Type the shift number:\n"))
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p',
+            'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z','a', 'b', 'c', 'd', 'e', 'f', 'g', 
+            'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
 
+#Define a new function insted of more than one function.
+
+def cipher(c_text,c_shift,c_direction):
+    end_text = ""
+    if c_direction == "decode":
+        c_shift *= -1
+        
+    for char in c_text:
+        if char in alphabet:
+            position = alphabet.index(char)
+            new_position = position + c_shift
+            end_text += alphabet[new_position]
+        else:
+            end_text += char
+        #TODO -1:> What happens if the user enters a number/symbol/space?
+        # Can you fix the code to keep the number/symbol/space when the text is encoded/decoded
+        #e.g. start_text = "meet me at 3"
+        #end_text = "....... ... ... 3"
+        
+    print(f"The {c_direction}d message is {end_text}.")
+
+
+
+#TODO -:> Can you figure out a way to ask the user if they want to restart the cipher program?
+#e.g. Type 'yes' if you want to go again. Otherwise type 'no'.
+# If they type 'yes then ask them for the direction/text/shift again and call the caesar() function 
+# again?
+# Hint: Try creating a new function that calls itself if they type 'yes'.
+should_continue = True
+while should_continue:
+    direction = input("Type 'encode' to encrypt, type 'decode'to decrypt:\n")
+    text = input("Type your message:\n").lower()
+    shift = int(input("Type the shift number:\n"))
+    ##todo- if user input shift number grater than 26.
+    shift = shift % 26
+
+    cipher(c_direction=direction,c_shift=shift,c_text=text)
+    
+    result = input("Type 'yes' for continue or 'no' to end. \n")
+    
+    if result == 'no':
+        should_continue = False
+        print("Good Bye!")
+
+
+
+
+
+
+
+
+
+
+
+
+##################################################################################################
 #TODO-1: Create a function called 'encrypt' that takes the 'text' and 'shift' as inputs.
 
-def encrypt(plain_text,shift_num):
-    cipher_text = ''
-    for letter in plain_text:
-        position = alphabet.index(letter)
-        new_position = position + shift_num
-        new_letter = alphabet[new_position]
-        cipher_text += new_letter
-    print(f"The encoded message is {cipher_text}")
+# def encrypt(plain_text,shift_num):
+#     cipher_text = ''
+#     for letter in plain_text:
+#         position = alphabet.index(letter)
+#         new_position = position + shift_num
+#         new_letter = alphabet[new_position]
+#         cipher_text += new_letter
+    # print(f"The encoded message is {cipher_text}")
 
 
     #TODO-2: Inside the 'encrypt' function, shift each letter of the 'text' forwards in the alphabet by the shift amount and print the encrypted text.  
@@ -35,14 +89,14 @@ def encrypt(plain_text,shift_num):
 
 #TODO -1:> Create a different function called 'decrypt' that takes the 'text' and 'shift' as inputs.
 
-def decrypt(cipher_text, d_shift):
-    d_text = ''
-    for letter in cipher_text:
-        position = alphabet.index(letter)
-        new_position = position - d_shift
-        d_text += alphabet[new_position]
+# def decrypt(cipher_text, d_shift):
+#     d_text = ''
+#     for letter in cipher_text:
+#         position = alphabet.index(letter)
+#         new_position = position - d_shift
+#         d_text += alphabet[new_position]
         
-    print(f"The decoded text is {d_text} ")
+    # print(f"The decoded text is {d_text} ")
     
 
         #TODO -2:> Inside the 'decrypt' function, shift each letter of the 'text' *backword* in the alphabet by the shift
@@ -57,7 +111,7 @@ def decrypt(cipher_text, d_shift):
         #   call the correct function based on that 'direction' variable. You should be able to test the code to 
         #   encrypt *and* decrypt a message.
 
-if direction == "encode":
-    encrypt(plain_text=text,shift_num=shift)
-elif direction == "decode":
-    decrypt(cipher_text=text,d_shift=shift)
+# if direction == "encode":
+#     encrypt(plain_text=text,shift_num=shift)
+# elif direction == "decode":
+#     decrypt(cipher_text=text,d_shift=shift)
